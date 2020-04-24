676b15a087586087c56801c5feb93251511a9061
alexhruzd <alex.gaby77@gmail.com>
2020-04-24 18:51:40 +0300

    correct slider settings

--- js/lesuorac.js
+++ js/lesuorac.js
@@ -1,6 +1,3 @@
-let moveNum = 0;
-let timerAautoplay;
-
 const defaultSliderSettings = {
     slideWidth: 300,
     slideHeight: '',
@@ -10,47 +7,79 @@ const defaultSliderSettings = {
     timeout: 3000,
     navs: true,
     transitionDelay: 800,
-    dots: true
+    dots: true,
+    stopOnHover: true,
+    margin: 10
 }
 
 let arraySlider = document.querySelectorAll('.lesuorac_slider');
 
 arraySlider.forEach(slider => {
     slider.slider = function (sliderSettings = {}) {
+        //let moveNum = 0;
+        let timerAautoplay;
 
         for (key in defaultSliderSettings)
             if (sliderSettings[key] === undefined)
                 sliderSettings[key] = defaultSliderSettings[key];
 
-        const slidesOffset = sliderSettings.slideWidth /* * sliderSettings.maxSlide*/;
+        const slidesOffset = sliderSettings.slideWidth + sliderSettings.margin /* * sliderSettings.maxSlide*/;
 
-        createSlider(this, sliderSettings);
+        createSlider(slider, sliderSettings);
 
         let nextSlideBtn = slider.querySelector('.next_slide');
         let prevSlideBtn = slider.querySelector('.prev_slide');
+        let navs = slider.querySelector('.navs');
+        let dotsBlock = slider.querySelector('.dots_block');
+
+        setActiveDot(0);
 
         nextSlideBtn.addEventListener('click', nextSlideClick);
         prevSlideBtn.addEventListener('click', prevSlideClick);
 
         if (!sliderSettings.navs) {
-            this.querySelector('.navs').remove();
+            navs.remove();
             sliderSettings.loop = true;
             sliderSettings.autoplay = true;
+        } else {
+            if (sliderSettings.dots) {
+                dotsBlock.addEventListener('click', (event) => {
+                    event.preventDefault();
+
+                    if (event.target.tagName == 'A') {
+                        let numSlide = event.target.dataset.numSlide;
+                        dotSlideClick(numSlide);
+                    }
+                });
+            }
         }
 
-        if (sliderSettings.autoplay) {
-            sliderSettings.loop = true;
-            timerAautoplay = setInterval(() => nextSlideClick(), sliderSettings.timeout);
-        }
-
-        let slides = this.querySelectorAll('div.one_slide');
-        let slideCount = slides.length;
-
-        let counter = 0;
+        startAutoPlay();
 
         let innerContainer = slider.querySelector('.inner_container');
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        if (sliderSettings.stopOnHover && sliderSettings.autoplay) {
+            innerContainer.addEventListener('mouseover', stopAutoPlay);
+            innerContainer.addEventListener('mouseout', () => { startAutoPlay(sliderSettings.transitionDelay) });
+        }
+
+        let slides = slider.querySelectorAll('div.one_slide');
+        let slideCount = slides.length;
+        let counter = 0;
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        function startAutoPlay(timeout = sliderSettings.timeout) {
+            if (sliderSettings.autoplay) {
+                sliderSettings.loop = true;
+                timerAautoplay = setInterval(() => nextSlideClick(), timeout);
+            }
+        }
+
+        function stopAutoPlay() {
+            if (timerAautoplay !== undefined)
+                clearInterval(timerAautoplay);
+        }
+
         function firstSlide() {
             let curSlides = slider.querySelectorAll('div.one_slide');
             return curSlides[0];
@@ -61,11 +90,38 @@ arraySlider.forEach(slider => {
             return curSlides[curSlides.length - 1];
         }
 
-        function nextSlideClick(event) {
-            if (event != undefined) event.preventDefault();                
+        function getSlide(numSlide) {
+            let curSlides = slider.querySelectorAll('div.one_slide');
+            return curSlides[numSlide];
+        }
+
+        function getSlideCount() {
+            return slider.querySelectorAll('div.one_slide').length;
+        }
+
+        function dotSlideClick(numClickSlide) {
+            console.log('counter=', counter, ' click=', numClickSlide);
+            if (numClickSlide > counter) {
+                nextSlideClick(undefined, numClickSlide - counter);
+            }
+
+            if (numClickSlide < counter) {
+                prevSlideClick(undefined, counter - numClickSlide);
+            }
+        }
+
+        function setActiveDot(numDot) {
+            for (const dotIt of dotsBlock.children) {
+                dotIt.classList.remove('active');
+            }
+
+            dotsBlock.children[numDot].classList.add('active');
+        }
+
+        function nextSlideClick(event, numMoveSlides = 1) {
+            if (event != undefined) event.preventDefault();
 
             if (!sliderSettings.loop && counter >= slideCount - sliderSettings.maxSlide) {
-                //nextSlideBtn.classList.add('disabled');
                 return;
             }
 
@@ -73,25 +129,35 @@ arraySlider.forEach(slider => {
                 counter = -1;
             }
 
-            counter++;
-            //console.log(counter);
+            counter += numMoveSlides;
+            setActiveDot(counter);
 
-            innerContainer.innerHTML = innerContainer.innerHTML + '\n' + firstSlide().outerHTML;
+            stopAutoPlay();
+
+            for (let i = 0; i < numMoveSlides; i++) {
+                innerContainer.innerHTML += '\n' + getSlide(i).outerHTML;
+            }
 
             innerContainer.classList.remove('no_transition');
-            nextSlideBtn.classList.add('no_click');
+            navs.classList.add('no_click');
 
-            innerContainer.style.transform = `translate(${-slidesOffset}px, 0)`;
+            innerContainer.style.transform = `translate(${-slidesOffset * numMoveSlides}px, 0)`;
 
             setTimeout(() => {
                 innerContainer.classList.add('no_transition');
-                firstSlide().remove();
+
+                for (let i = 0; i < numMoveSlides; i++) {
+                    firstSlide().remove();
+                }
+
                 innerContainer.style.transform = `translate(0, 0)`;
-                nextSlideBtn.classList.remove('no_click');
+                navs.classList.remove('no_click');
             }, sliderSettings.transitionDelay);
+
+            startAutoPlay();
         }
 
-        function prevSlideClick(event) {
+        function prevSlideClick(event, numMoveSlides = 1) {
             if (event != undefined) event.preventDefault();
 
             if (counter <= 0)
@@ -100,29 +166,37 @@ arraySlider.forEach(slider => {
                 } else {
                     counter = slideCount;
                 }
-            counter--;
-            //console.log(counter);
+            counter -= numMoveSlides
+            setActiveDot(counter);
+
+            stopAutoPlay();
 
             innerContainer.classList.add('no_transition');
-            innerContainer.innerHTML = lastSlide().outerHTML + '\n' + innerContainer.innerHTML;
-            innerContainer.style.transform = `translate(${-slidesOffset}px, 0)`;
+
+            for (let i = 0; i < numMoveSlides; i++) {
+                innerContainer.innerHTML = getSlide(getSlideCount() - i - 1).outerHTML + '\n' + innerContainer.innerHTML;
+            }
+
+            innerContainer.style.transform = `translate(${-slidesOffset * numMoveSlides}px, 0)`;
 
             setTimeout(() => {
                 innerContainer.classList.remove('no_transition');
-
-                prevSlideBtn.classList.add('no_click');
-
+                navs.classList.add('no_click');
                 innerContainer.style.transform = `translate(0, 0)`;
 
                 setTimeout(() => {
-                    lastSlide().remove();
-                    prevSlideBtn.classList.remove('no_click');
+                    for (let i = 0; i < numMoveSlides; i++) {
+                        lastSlide().remove();
+                    }
+                    navs.classList.remove('no_click');
                 }, sliderSettings.transitionDelay);
             });
 
+            startAutoPlay();
         }
     }
 });
+
 
 function createSlider(slider, sliderSettings) {
 
@@ -131,6 +205,7 @@ function createSlider(slider, sliderSettings) {
         item.classList.add('one_slide');
         item.style.width = `${sliderSettings.slideWidth}px`;
         item.style.height = `${sliderSettings.slideHeight}px`;
+        item.style.marginRight = `${sliderSettings.margin}px`;
     });
 
     slider.innerHTML = `
@@ -141,6 +216,7 @@ function createSlider(slider, sliderSettings) {
         </div>
         <div class="navs">
             <a href="#" class="prev_slide"><</a>
+            ${ sliderSettings.dots ? createDots() : ''}
             <a href="#" class="next_slide">></a>
         </div>
     `;
@@ -148,7 +224,21 @@ function createSlider(slider, sliderSettings) {
     let outerContainer = slider.querySelector('.outer_container');
     outerContainer.style.width = `${sliderSettings.slideWidth * sliderSettings.maxSlide}px`;
     let innerContainer = slider.querySelector('.inner_container');
-    innerContainer.style.width = `${sliderSettings.slideWidth * (oneSlideArr.length) + sliderSettings.slideWidth}px`;
+    innerContainer.style.width = `${(sliderSettings.slideWidth * oneSlideArr.length + sliderSettings.margin) * 2}px`;
 
     innerContainer.style.transition = `transform ${sliderSettings.transitionDelay}ms`;
+
+    function createDots() {
+        let dotBlock = '';
+
+        for (let i = 0; i < oneSlideArr.length; i++) {
+            dotBlock += `<a href="#" class="dot" data-num-slide="${i}"></a>\n`
+        }
+
+        return `
+            <div class="dots_block">
+            ${dotBlock}
+            </div>
+        `;
+    }
 }
